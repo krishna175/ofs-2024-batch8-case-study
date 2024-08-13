9323a6f8cdbdd3d95d4881493524c24de5d9d0c4
Harikrishnan <sathyanharikrishnan@gmail.com>
2024-08-13 14:07:53 +0530

    login completed

--- Oracle_Banking_Project/OracleBanking/src/main/java/com/ofss/main/controller/LoginController.java
+++ Oracle_Banking_Project/OracleBanking/src/main/java/com/ofss/main/controller/LoginController.java
@@ -5,6 +5,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,17 +24,25 @@ public class LoginController {
 	LoginService loginService;
 	
 //	http://localhost:8080/userlogin/loginstatus
-	@GetMapping("/loginstatus/{username}/{password}")
-	public ResponseEntity<String> validateLogin(@PathVariable("username") String username,@PathVariable("password") String password){
-		LoginDetails login = new LoginDetails(0, username, password);
-		int customer = loginService.loginValidation(login);
-		System.out.println(customer);
-		if (customer > 0) {
-            return ResponseEntity.ok("Login successful");
-        } else {
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid username or password");
-        }
+//	@GetMapping("/loginstatus/{username}/{password}")
+//	public ResponseEntity<String> validateLogin(@PathVariable("username") String username,@PathVariable("password") String password){
+//		LoginDetails login = new LoginDetails(0, username, password);
+//		int customer = loginService.loginValidation(login);
+//		System.out.println(customer);
+//		if (customer > 0) {
+//            return ResponseEntity.ok("Login successful");
+//        } else {
+//            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid username or password");
+//        }
+//	}
+	
+	@PostMapping("loginstatus")
+	public int validateUserLogin(@RequestBody LoginDetails loginDetails) {
+		LoginDetails login = new LoginDetails(0,loginDetails.getUsername(),loginDetails.getPassword());
+		int customerId = loginService.loginValidation(login);
+		return customerId;
 	}
+	
 	
 	
 }	
---
+++ Oracle_Banking_Project/OracleBanking/src/main/java/com/ofss/main/entity/CustomerDetails.java
@@ -0,0 +1,17 @@
+package com.ofss.main.entity;
+
+public class CustomerDetails {
+	
+	private int customeId;
+	private int customer_id;
+	private String first_name;
+	private String last_name;
+	private String address1;
+	private String address2;
+	private String address3;
+	private String city;
+	private String state;
+	private String zip;
+	private String phone;
+	private String email;
+}
